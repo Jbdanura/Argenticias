89c3ea770431f106fa063f4caeb697d1c06f7922
unknown <danurabe@gmail.com>
2022-06-24 00:29:05 -0300

    last css changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 import {useState,useEffect} from "react"
 import axios from "axios"
-import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom"
+import {BrowserRouter as HashRouter, Route, Link, Routes} from "react-router-dom"
 import Home from "./components/Home"
 import Category from './components/Category';
 
 function App() {
   const [news,setNews] = useState([])
-  const [navMobile, setNavMobile] = useState(false)
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
   useEffect(()=>{
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
-    }
 
-    window.addEventListener('resize', changeWidth)
 
     const promise = axios.get("https://argenticias.herokuapp.com/news/all")
     promise.then(response => {
@@ -23,36 +17,31 @@ function App() {
     })
   },[])
 
-  const navMobileStyle =  {
-    display: "block",
-    marginLeft: "40px",
-    marginTop: "20px"
-  }
+
 
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <div className="nav">
           <a href="/" className="logo">
             <span style={{color:"lightblue"}}>Arg</span><span>e</span><span style={{color:"lightyellow"}}>nti</span>
             <span>c</span><span style={{color:"lightblue"}}>ias</span>
           </a>
-          <div className="links" style={screenWidth > 850 ? {display:"flex"} : !navMobile ? {display: "none"} : navMobileStyle}>
-            <Link to="/" className={navMobile ? "link-mobile": null}>General</Link> 
-            <Link to="/business" className={navMobile ? "link-mobile": null}>Economia</Link>
-            <Link to="/entertainment" className={navMobile ? "link-mobile": null} >Entretenimiento</Link>
-            <Link to="/health" className={navMobile ? "link-mobile": null}>Salud</Link>
-            <Link to="/science" className={navMobile ? "link-mobile": null}>Ciencia</Link>
-            <Link to="/sports" className={navMobile ? "link-mobile": null}>Deportes</Link>
-            <Link to="/technology" className={navMobile ? "link-mobile": null}>Tecnologia</Link>
+          <div className="links">
+            <Link to="/" >General</Link> 
+            <Link to="/business" >Economia</Link>
+            <Link to="/entertainment" >Entretenimiento</Link>
+            <Link to="/health" >Salud</Link>
+            <Link to="/science" >Ciencia</Link>
+            <Link to="/sports" >Deportes</Link>
+            <Link to="/technology" >Tecnologia</Link>
           </div>
-        <a onClick={()=>setNavMobile(!navMobile)} className="menu" style={screenWidth > 850 ? {display:"none"} :{display:"block"}}>☰</a>
         </div>
         <Routes>
           <Route path="/" element={<Home articles={news}/>}/>
           <Route path="/:category" element={<Category/>}/>
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
